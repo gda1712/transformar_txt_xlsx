f2ff92b590021ceef21de6db5922eca2cffd998d
Gabriel Díaz <gda.m1712@gmail.com>
2020-06-24 10:22:39 -0400

    Ahora se puede usar el nombre del archivo que el usuario proporciones

--- transformar_valores_excel.py
+++ transformar_valores_excel.py
@@ -2,34 +2,44 @@ from os.path import isfile
 from os import getcwd
 from openpyxl import Workbook
 
-#Revisamos que el txt existe
-if isfile(r"{}/datos.txt".format(getcwd())):
-    archivo = open(r"{}/datos.txt".format(getcwd()), "r")
+def main():
 
-    #Creamos el libro de excel
-    libro = Workbook()
-    hoja = libro["Sheet"]
+    #Pedimos el nombre el archivo
+    nombre_archivo = input("Ingrese el nombre del archivo con terminación .txt: ")
+    #Revisamos que el txt existe
+    if isfile(r"{}/{}".format(getcwd(), nombre_archivo)):
+        archivo = open(r"{}/{}".format(getcwd(), nombre_archivo), "r")
 
-    contador_fila = 1
-    #Recorremos el archivo
-    for linea in archivo:
-        valor = ""
-        #COntador que se encarga de llevar la columna actual
-        contador_columna = 1
+        #Creamos el libro de excel
+        libro = Workbook()
+        hoja = libro["Sheet"]
 
-        for caracter in linea:
-            if caracter == " " or caracter == "\n":
-                
-                #Ubicamos en la fila y columna el valor
-                hoja.cell(row=contador_fila, column=contador_columna, value=valor)
-                valor = ""
-                contador_columna += 1
-            else:
-                valor += caracter
-        
-        contador_fila += 1
+        contador_fila = 1
+        #Recorremos el archivo
+        for linea in archivo:
+            valor = ""
+            #COntador que se encarga de llevar la columna actual
+            contador_columna = 1
 
-    #Guardamos los archivos en datos.xlsx
-    libro.save(filename = "datos.xlsx")
+            for caracter in linea:
+                if caracter == " " or caracter == "\n":
+                    
+                    #Ubicamos en la fila y columna el valor
+                    hoja.cell(row=contador_fila, column=contador_columna, value=valor)
+                    valor = ""
+                    contador_columna += 1
+                else:
+                    valor += caracter
+            
+            contador_fila += 1
 
-    archivo.close()
+        #Guardamos los archivos en datos.xlsx
+        libro.save(filename = "datos.xlsx")
+
+        print("Convertido correctamente")
+        archivo.close()
+
+    else:
+        print("Error, no se encontro el archivo")
+
+main()
